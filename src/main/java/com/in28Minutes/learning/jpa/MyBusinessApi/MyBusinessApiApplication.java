280cdf7e87cf53be7889792d33802ce884d489f3
thierryfotso06 <thierryfotso06@gmail.com>
2020-07-31 14:18:27 +0200

    first commit

---
+++ src/main/java/com/in28Minutes/learning/jpa/MyBusinessApi/MyBusinessApiApplication.java
@@ -0,0 +1,13 @@
+package com.in28Minutes.learning.jpa.MyBusinessApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBusinessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBusinessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28Minutes/learning/jpa/MyBusinessApi/MyBusinessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28Minutes.learning.jpa.MyBusinessApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBusinessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
